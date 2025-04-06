839886cd4c57ac3ebb6dffc7842a6fee1c0f9970
Siim Raudsepp <siimraudsepp@msn.com>
2025-04-06 17:18:17 +0300

    Add initial project files

---
+++ DeathCounter/DeathCounter/DeathCounterModSystem.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Vintagestory.API.Common;
+using Vintagestory.API.Server;
+
+namespace DeathCounter;
+
+public record DeathDescriptor(string Reason, string DeathTimestamp);
+
+public record PlayerDeathData(int DeathCount, List<DeathDescriptor> Deaths)
+{
+    public int DeathCount { get; set; } = DeathCount;
+}
+
+public class DeathCounterModSystem : ModSystem
+{
+    private ICoreServerAPI _api;
+
+
+    public override void StartServerSide(ICoreServerAPI api)
+    {
+        _api = api;
+
+        _api.Event.PlayerDeath += (player, source) =>
+        {
+            Console.WriteLine(player.ServerData);
+            
+            if(!player.ServerData.CustomPlayerData.ContainsKey("PlayerDeaths"))
+            {
+                player.ServerData.CustomPlayerData.Add("PlayerDeaths",
+                    JsonUtil.ToPrettyString(new PlayerDeathData(0, new List<DeathDescriptor>())));
+            }
+           
+
+            var deathData = JsonUtil.FromString<PlayerDeathData>(player.ServerData.CustomPlayerData["PlayerDeaths"]);
+
+            deathData.Deaths.Add(new DeathDescriptor( source.Source.ToString(),DateTime.UtcNow.ToString("o")));
+            deathData.DeathCount = deathData.Deaths.Count;
+
+            player.ServerData.CustomPlayerData["PlayerDeaths"] = JsonUtil.ToString(deathData);
+        };
+    }
+}
